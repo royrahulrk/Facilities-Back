dfe6fcf2d420ef8da4317baba2bf980351fc7717
Rahul Kumar Meenaa <99737908+royrahulrk@users.noreply.github.com>
2023-07-12 17:20:57 +0530

    new

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 var bodyParser = require('body-parser')
-
+const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
